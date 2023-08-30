c542f22f8318d7e8640db792c571a932c4231a44
schaumann-byte <silva.vito1@gmail.com>
2023-08-29 23:22:35 -0300

    progresso do dia 22

--- day-22/main.py
+++ day-22/main.py
@@ -1,4 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
 
 # Create the Screen
 screen = Screen()
@@ -7,35 +11,47 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-# Create the paddle
-paddle = Turtle()
-paddle.color("white")
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+# Create the paddles
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
+# Create the ball
+ball = Ball()
 
-def move_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def move_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+# Create the scoreboard
+scoreboard = ScoreBoard()
 
 screen.listen()
-screen.onkey(move_up, "Up")
-screen.onkey(move_down, "Down")
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
+    ball.move()
+    time.sleep(ball.move_speed)
+
+    # Detect Collision with Horizontal Walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with right paddle or left paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
+    # Detect if R_Paddle misses
+    if ball.xcor() > 380:
+        ball.reset_round()
+        scoreboard.l_point()
 
-
+    # Detect if L_Paddle misses
+    if ball.xcor() < -380:
+        ball.reset_round()
+        scoreboard.r_point()
 
 
 
